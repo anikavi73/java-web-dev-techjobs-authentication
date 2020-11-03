30cd907fc7d7c0fd6d3b95071b68bf5a5e7d8e48
Kavitha <anikavi73@gmail.com>
2020-11-02 19:54:32 -0600

    Studio 19 updated

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/UserRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/UserRepository.java
@@ -1,4 +1,4 @@
-package org.launchcode.javawebdevtechjobsauthentication.data;
+package org.launchcode.javawebdevtechjobsauthentication.models.data;
 
 import org.launchcode.javawebdevtechjobsauthentication.models.User;
 import org.springframework.data.repository.CrudRepository;
